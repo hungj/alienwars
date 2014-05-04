dc7fc3d2005670ec3fbb92aca510f3faa58c5ad0
Jonathan Hung <jyhung2357@gmail.com>
2014-05-04 16:41:12 -0700

    good on chrome

--- geolocationTest/geolocation.js
+++ geolocationTest/geolocation.js
@@ -1,34 +1,34 @@
 
-
-	
-		/*var img = new Image();
-	img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=300x300&sensor=false";
-location.appendChild(img);
-*/
+var map;
 
 
 google.maps.event.addDomListener(window, 'load', function initialize() {
 	var latitude, longitude;
 	navigator.geolocation.getCurrentPosition(function(position) {
 		latitude = position.coords.latitude;
-		longitude = position.coords.longitude;
-		
+		longitude = position.coords.longitude;	
   var mapOptions = {
     zoom: 18,
     center: new google.maps.LatLng(latitude, longitude)
   };
   map = new google.maps.Map(document.getElementById('location'), mapOptions);
   document.getElementById('coords').innerHTML = latitude + " " + longitude;
-		//initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 	});
 
 });
 
-
-
-var watch = navigator.geolocation.watchPosition(function(position) {
-  //alert('blah');
-	var location = document.getElementById('location');
+var calibrate = 0;
+var positionOptions = {
+  maximumAge: 5000,
+//  timeout: 2000
+};
+var calibrate = 0;
+var watch = navigator.geolocation.watchPosition(setLocation, error, positionOptions);
+function setLocation(position) {
+  calibrate++;
+  if (calibrate < 2)
+    return;
+	var loc = document.getElementById('location');
 	/*https://developer.mozilla.org/en-US/docs/WebAPI/Using_geolocation
 	https://developers.google.com/maps/documentation/javascript/examples/marker-simple*/
 
@@ -37,40 +37,12 @@ var watch = navigator.geolocation.watchPosition(function(position) {
 		zoom: 18,
 		center: pos
 	};
-	var map = new google.maps.Map(location, options);
-
+	//var map = new google.maps.Map(loc, options);
+  document.getElementById("coords").innerHtml += "<br/>"+ position.coords.latitude + " " + position.coords.longitude
 	var marker = new google.maps.Marker({
 		position: pos,
 		map: map
 	});
-});/*
-var watchID = navigator.geolocation.watchPosition(function(position) {
-  do_something(position.coords.latitude, position.coords.longitude);
-});
 
-  var output = document.getElementById("out");
-
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);*/
+}
+function error() {alert('error');}
