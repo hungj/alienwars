0d27afd68f2cc039c439058d1304c500ea39d122
Joanna Kim <joannakim13@gmail.com>
2014-05-20 15:54:11 -0700

    Adding CORRECT file

--- touch.js
+++ touch.js
@@ -1,48 +1,61 @@
-var posX=0, posY=0,
-	lastPosX=0, lastPosY=0,
-	bufferX=0, bufferY=0,
-    scale=1, last_scale,
-    rotation= 1, last_rotation, dragReady=0;
+var touchArea = document.getElementById("imageWrapper");
+var elemImage = document.getElementById("image");
 
-var image = document.getElementById('image');
+var posX=0, posY=0, 
+    lastPosX=0, lastPosY=0, 
+    bufferX=0, bufferY=0, 
+    scale=1, last_scale, scale_limit=10, 
+    rotation=1, last_rotation, dragReady=0;
 
-var hammertime = Hammer(image).on('touch drag transform', function(event) {
-	elemImage = document.getElementById('image');
-	manageMultitouch(event);
+var options = {
+    transform_always_block: true,
+    preventDefault: true,
+    transform_min_scale: 1,
+    drag_block_horizontal: true,
+    drag_block_vertical: true,
+    drag_min_distance: 0
+};
+
+var hammertime = Hammer(touchArea, options).on("touch drag dragend transform transformend", function(event) {
+    manageMultiTouch(event);
 });
 
-function manageMultitouch(event){
-	switch(event.type) {
-		case 'touch':
-            last_scale = scale;
-            last_rotation = rotation;
-            break;
- 
-        case 'drag':
-            posX = ev.gesture.deltaX + lastPosX;
-			posY = ev.gesture.deltaY + lastPosY;
-            break;
- 
-        case 'transform':
-            rotation = last_rotation + ev.gesture.rotation;
-            scale = Math.max(1, Math.min(last_scale * ev.gesture.scale, 10));
-            break;
- 
-        case 'dragend':
-            lastPosX = posX;
-            lastPosY = posY;
-            break;
-        }
-	}
-	
-	var transform =
-        "translate3d("+posX+"px,"+posY+"px, 0) " +
-        "scale3d("+scale+","+scale+", 0) " +
-        "rotate("+rotation+"deg) ";
- 
+function manageMultiTouch(event) {
+    switch(event.type) {
+	case "touch":
+	     last_scale = scale;
+	     last_rotation = rotation;
+	     break;
+
+	case "transform":
+	    rotation = last_rotation + event.gesture.rotation;
+	    scale = event.gesture.scale * last_scale;
+	    break;
+
+	case "transformend":
+	    event.gesture.stopDetect();
+	    break;
+
+	case "drag":
+                 posX = event.gesture.deltaX + lastPosX;
+                 posY = event.gesture.deltaY + lastPosY;
+
+        break;
+
+	case "dragend":
+	    lastPosX = posX;
+	    lastPosY = posY;
+	    break;
+    }
+
+    var transform = 
+	"translate3d(" + posX + "px, " + posY + "px, 0)" +
+	"scale3d(" + scale + ", " + scale + ", 0)" +
+	"rotate(" + rotation + "deg)";
+
     elemImage.style.transform = transform;
     elemImage.style.oTransform = transform;
     elemImage.style.msTransform = transform;
     elemImage.style.mozTransform = transform;
     elemImage.style.webkitTransform = transform;
-}
+};
